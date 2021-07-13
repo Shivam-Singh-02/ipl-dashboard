ac63256a2cf0a45e6893d58f2779e075e641fcc6
Shivam Singh <shivamsingh@Shivams-MacBook-Air.local>
2021-07-13 22:40:05 +0530

    React app with Team page

---
+++ src/frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './pages/TeamPage'
+import { TeamPage } from './pages/TeamPage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TeamPage/>
+    </div>
+  );
+  
+}
+
+export default App;
